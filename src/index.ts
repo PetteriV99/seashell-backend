c685c03bf7438021eab626bac74336a6e1ec0954
PetteriV99 <petterivanttinen99@gmail.com>
2023-11-22 23:52:24 +0200

    Add logging

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,4 @@ app.use(express.json());
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 })
-.on('error', (error) => logger.error(error));
+.on('error', (error: Error) => logger.error(error));
