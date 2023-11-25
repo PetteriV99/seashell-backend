a578aac8a774078106307488c873a9600ee5fcd8
PetteriV99 <petterivanttinen99@gmail.com>
2023-11-25 18:47:48 +0200

    Change console log to winstons

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import logger from './utils/logger';
 
 app
   .listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+    logger.info(`Server running on port ${process.env.PORT}`);
   })
   .on('error', (error: Error) => logger.error(error));
